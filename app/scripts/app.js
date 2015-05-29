c98f6bb102a4cc4af4fec06cfad612eb8c188a0c
binbin <845040571@qq.com>
2015-05-29 17:48:16 +0800

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mytodoApp
+ * @description
+ * # mytodoApp
+ *
+ * Main module of the application.
+ */
+
+angular.module('mytodoApp', [
+   'ngCookies',
+   'ngResource',
+   'ngSanitize',
+   'ngRoute',
+   'ui.sortable',
+   'LocalStorageModule'
+])
+   .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+       localStorageServiceProvider.setPrefix('ls');
+   }])
+   .config(function ($routeProvider) {
+       $routeProvider
+   .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+   })
+   .otherwise({
+      redirectTo: '/'
+   });
+ });
